9878557249ef76a9f31f4dcbd744d91628fa441a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-24 13:27:40 +0530

    Launcher3: Add recents shortcut for floating window
    using LMOFreeform.
    
    Change-Id: Ia6da8c4a61a2c14e1f7750994bd25961e47f5e9d
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -113,6 +113,7 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
     private static final TaskShortcutFactory[] MENU_OPTIONS = new TaskShortcutFactory[]{
             TaskShortcutFactory.APP_INFO,
             TaskShortcutFactory.SPLIT_SELECT,
+            TaskShortcutFactory.FLOATING,
             TaskShortcutFactory.UNINSTALL,
             TaskShortcutFactory.PIN,
             TaskShortcutFactory.INSTALL,
