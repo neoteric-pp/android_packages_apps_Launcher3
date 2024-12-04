f769a02dfa7ad980910792825740c061134f2298
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-04 08:52:33 +0800

    Launcher3: Integrate LMOFreeform to system shortcut
    @neobuddy89: Rename this to FREE_FORM to FLOATING following TaskShortcutFactory.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -47,6 +47,7 @@ import static com.android.launcher3.popup.QuickstepSystemShortcut.getSplitSelect
 import static com.android.launcher3.popup.SystemShortcut.APP_INFO;
 import static com.android.launcher3.popup.SystemShortcut.BUBBLE_SHORTCUT;
 import static com.android.launcher3.popup.SystemShortcut.DONT_SUGGEST_APP;
+import static com.android.launcher3.popup.SystemShortcut.FLOATING;
 import static com.android.launcher3.popup.SystemShortcut.INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.PAUSE_APPS;
 import static com.android.launcher3.popup.SystemShortcut.PRIVATE_PROFILE_INSTALL;
@@ -493,6 +494,7 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer,
         shortcuts.addAll(getSplitShortcuts());
         shortcuts.add(UNINSTALL);
         shortcuts.add(PAUSE_APPS);
+        shortcuts.add(FLOATING);
         shortcuts.add(WIDGETS);
         shortcuts.add(INSTALL);
         if (Flags.enablePrivateSpaceInstallShortcut()) {
