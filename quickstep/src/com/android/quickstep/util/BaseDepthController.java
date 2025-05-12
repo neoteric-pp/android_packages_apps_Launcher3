acd5a04e942250afde13a6e0b2ae9c9cb8e02234
vinayjoglekar <vinayjoglekar@google.com>
2025-05-12 14:28:07 +0100

    Fix blur jank when app -> overview and app -> home.
    This cl fixes conditions of when to skip the blur. While setting any  new surface, we should always apply the blur.
    
    Fix: 417131537
    Fix: 416689392
    Test: Go to overview from app. Go to home from app
    Flag: com.android.launcher3.enable_overview_background_wallpaper_blur
    Change-Id: I16ff00cf0be554eaf9aa4f48adc23b5cec4ae3da

--- quickstep/src/com/android/quickstep/util/BaseDepthController.java
+++ quickstep/src/com/android/quickstep/util/BaseDepthController.java
@@ -147,7 +147,7 @@ public class BaseDepthController {
     protected void onInvalidSurface() { }
 
     protected void applyDepthAndBlur() {
-        applyDepthAndBlur(null, /* applyImmediately */ false);
+        applyDepthAndBlur(null, /* applyImmediately */ false, /* skipSimilarBlur */ true);
     }
 
     /**
@@ -157,14 +157,14 @@ public class BaseDepthController {
      * @param applyImmediately whether to apply the blur immediately or defer to the next frame.
      */
     protected void applyDepthAndBlur(SurfaceControl.Transaction transaction,
-            boolean applyImmediately) {
+            boolean applyImmediately, boolean skipSimilarBlur) {
         try (transaction) {
-            applyDepthAndBlurInternal(transaction, applyImmediately);
+            applyDepthAndBlurInternal(transaction, applyImmediately, skipSimilarBlur);
         }
     }
 
     private void applyDepthAndBlurInternal(SurfaceControl.Transaction transaction,
-            boolean applyImmediately) {
+            boolean applyImmediately, boolean skipSimilarBlur) {
         float depth = mDepth;
         IBinder windowToken = mLauncher.getRootView().getWindowToken();
         if (windowToken != null) {
@@ -209,8 +209,6 @@ public class BaseDepthController {
         int previousBlur = mCurrentBlur;
         int newBlur = !hasOpaqueBg ? (int) (blurAmount * mMaxBlurRadius) : 0;
         int delta = Math.abs(newBlur - previousBlur);
-        // Don't skip blur if a transaction is provided.
-        boolean skipSimilarBlur = transaction == null;
         if (skipSimilarBlur && delta < Utilities.dpToPx(1) && newBlur != 0 && previousBlur != 0) {
             Log.d(TAG, "Skipping small blur delta. newBlur: " + newBlur + " previousBlur: "
                     + previousBlur + " delta: " + delta + " surface: " + blurSurface);
@@ -292,7 +290,7 @@ public class BaseDepthController {
             mBaseSurfaceOverride = baseSurfaceOverride;
             Log.d(TAG, "setBaseSurfaceOverride: applying blur behind leash " + baseSurfaceOverride);
             SurfaceControl.Transaction transaction = setupBlurSurface();
-            applyDepthAndBlur(transaction, applyImmediately);
+            applyDepthAndBlur(transaction, applyImmediately, /* skipSimilarBlur */ false);
         }
     }
 
@@ -334,7 +332,8 @@ public class BaseDepthController {
             if (enableOverviewBackgroundWallpaperBlur()) {
                 transaction = setupBlurSurface();
             }
-            applyDepthAndBlur(transaction, /* applyImmediately */ false);
+            applyDepthAndBlur(transaction, /* applyImmediately */ false,
+                    /* skipSimilarBlur */ false);
         }
     }
 
